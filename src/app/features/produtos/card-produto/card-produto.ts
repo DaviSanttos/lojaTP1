b6951d9ca1b9bf949fca91727c509a756a4d97e4
DaviSanttos <davicels.santos@hotmail.com>
2025-09-11 21:47:09 -0300

    aula3

--- src/app/features/produtos/card-produto/card-produto.ts
+++ src/app/features/produtos/card-produto/card-produto.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
+import { Produto } from '../../../model/produto';
 
 @Component({
   selector: 'app-card-produto',
@@ -7,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './card-produto.css'
 })
 export class CardProduto {
+  produto = input.required<Produto>();
 
+  view = output<number>();
+  add = output<{ id: number; quantity: number }>();
+
+  qtde = signal(1);
+
+  onView() {
+    this.view.emit(this.produto().id);
+  }
+
+  onAdd() {
+    this.add.emit({ id: this.produto().id, quantity: this.qtde() });
+  }
 }
