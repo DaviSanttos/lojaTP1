bbe11fc43b19e0b08fb7efb8730eceabfe5b8c2e
DaviSanttos <davicelso.santos@hotmail.com>
2025-09-13 12:07:52 -0300

    refactor: clean up app component structure and improve readability

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from "./core/header/header";
 import { Footer } from "./core/footer/footer";
 import { Banner } from "./core/banner/banner";
+import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+import { CardProduto } from './features/produtos/card-produto/card-produto';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, Banner],
+  imports: [Header, Footer, Banner, ListaProdutos, CardProduto],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
